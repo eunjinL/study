0f9f85f8e7d359d899c3cee6c8c12c1a4d485ce6
eunjinL <ejlee@ati2000.co.kr>
2023-08-02 15:04:54 +0900

    Type Bacis/Instance vs Static Members
    2차

--- chapter2/chapter2/Program.cs
+++ chapter2/chapter2/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace chapter2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Test
+	{
+		public class Dog
+		{
+			public string Name;             // 인스턴스 필드 
+			public static int Population;   // 정적 필드 
+
+			public Dog(string n)           // 생성자 
+			{
+				Name = n;                      // 인스턴스 필드에 값 할당 
+				Population = Population + 1;   // 정적 필드값 1 증가 
+			}
+		}
+
+		static void Main()
+		{
+			Dog p1 = new Dog("Luda");
+			Dog p2 = new Dog("Mori");
+
+			Console.WriteLine(p1.Name);
+			Console.WriteLine(p2.Name);
+
+			Console.WriteLine(Dog.Population);   // 2
+		}
+	}
 }

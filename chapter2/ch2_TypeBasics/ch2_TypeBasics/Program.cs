bd3b56d2c837ed3b3887a84778df77ca4fd58058
eunjinL <ejlee@ati2000.co.kr>
2023-08-02 16:28:24 +0900

    Type Bacis/참조 형식
    값, 참조 중 참조 (class)

--- chapter2/ch2_TypeBasics/ch2_TypeBasics/Program.cs
+++ chapter2/ch2_TypeBasics/ch2_TypeBasics/Program.cs
@@ -8,24 +8,24 @@ namespace ch2_TypeBasics
 {
     class Program
     {
-        public struct Point
+        public class Point
         {
             public int x, y;
         }
-        static void Main()
-        {
-            Point p1 = new Point();
-            p1.x = 7;
+		static void Main()
+		{
+			Point p1 = new Point();
+			p1.x= 7;
 
-            Point p2 = p1;             
+			Point p2 = p1;             // 참조로 복사 
 
-            Console.WriteLine(p1.x);  // 7
-            Console.WriteLine(p2.x);  // 7
+			Console.WriteLine(p1.x);  // 7
+			Console.WriteLine(p2.x);  // 7
 
-            p1.x = 9;                  // p1의 x값 변경
+			p1.x = 9;                  // P1의 x만 변경
 
-            Console.WriteLine(p1.x);  // 9
-            Console.WriteLine(p2.x);  // 7
-        }
-    }
+			Console.WriteLine(p1.x);  // 9
+			Console.WriteLine(p2.x);  // 9
+		}
+	}
 }

5d4e2e2404b42cc27c6786a8e47db321d96c8227
eunjinL <ejlee@ati2000.co.kr>
2023-08-07 17:04:08 +0900

    0807
    chapter 3 공부 시작

---
+++ chapter3/chapter3/abstract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace chapter3
+{
+    class Program // 클래스 이름을 Program으로 변경
+    {
+        static void Main(string[] args) // args 추가
+        {
+            var stock = new Stock { SharesOwned = 200, CurrentPrice = 123.45M };
+            Console.WriteLine(stock.NetValue);
+        }
+
+        public abstract class Asset // Note abstract keyword
+        {
+            public abstract decimal NetValue { get; } // Note empty implementation
+        }
+
+        public class Stock : Asset
+        {
+            public long SharesOwned;
+            public decimal CurrentPrice;
+
+            // Override like a virtual method.
+            public override decimal NetValue => CurrentPrice * SharesOwned;
+        }
+    }
+}
---
+++ chapter3/chapter3/inheritance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter3
+{
+    class inheritance
+    {
+        public class Asset
+        {
+            public string Name;
+        }
+
+        public class Stock : Asset   
+        {
+            public long SharesOwned;
+        }
+
+        public class House : Asset  
+        {
+            public decimal Mortgage;
+        }
+
+        static void Main()
+        {
+            Stock msft = new Stock { Name = "MSFT", SharesOwned = 1000 };
+
+            Console.WriteLine(msft.Name);         // MSFT
+            Console.WriteLine(msft.SharesOwned);  // 1000
+
+            House mansion = new House { Name = "Mansion", Mortgage = 250000 };
+
+            Console.WriteLine(mansion.Name);      // Mansion
+            Console.WriteLine(mansion.Mortgage);  // 250000
+        }
+    }
+}

05686246b0425e303e3da7725336a498a7672ea0
eunjinL <ejlee@ati2000.co.kr>
2023-08-10 10:44:43 +0900

    0810
    - 기본적인 사칙연산 구현
    - 소수점, 부호 변경 가능
    - 분수와 제곱 기능 추가 예정 (UI만 구성해둠)

---
+++ chapter4/chapter4/throwing_exception.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter4
+{
+    class throwing_exception
+    {
+		static void Display(string name)
+		{
+			if (name == null)
+				throw new ArgumentNullException(nameof(name));
+
+			Console.WriteLine(name);
+		}
+
+		static void Main()
+		{
+			try
+			{
+				Display(null);
+			}
+			catch (ArgumentNullException ex)
+			{
+				Console.WriteLine("Caught the exception");
+			}
+		}
+	}
+}
---
+++ chapter4/chapter4/try_catch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter4
+{
+    class try_catch
+    {
+		static int Calc(int x) { return 10 / x; }
+
+		static void Main()
+		{
+			try
+			{
+				int y = Calc(0);
+				Console.WriteLine(y);
+			}
+			catch (DivideByZeroException ex)
+			{
+				Console.WriteLine("x cannot be zero");
+			}
+			Console.WriteLine("program completed");
+		}
+	}
+}
